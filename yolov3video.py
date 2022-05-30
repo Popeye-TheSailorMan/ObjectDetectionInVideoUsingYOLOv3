397b96f07601115d542be61d21041a2eb88f59f0
Abhisheksaxena <44522820+Abhisheksax@users.noreply.github.com>
2022-05-31 00:44:13 +0530

    Added result video + Complete code

--- yolov3video.py
+++ yolov3video.py
@@ -31,6 +31,7 @@ colours = np.random.randint(0,255,size=(len(labels),3),dtype='uint8')
 f = 0
 t = 0
 h,w = None, None
+writer = None
 
 #Getting frame one by one
 #Implementing Blob
@@ -66,8 +67,34 @@ while True:
             class_current = np.argmax(scores)
             confidence_current = scores[class_current]
 
-            if confidence_current>probability_minimum:
-                box_current = detected_objects[0:4]*np.array([w,h,w,h])
-                x_center, y_center_box_width,box_width = box_current
+            if confidence_current>minimum_probability:
+                box_current = detected_objects[0:4] * np.array([w,h,w,h])
+                x_center, y_center,box_width,box_height = box_current
                 x_min = int(x_center - (box_width / 2))
                 y_min = int(y_center - (box_height / 2))
+
+                bounding_boxes.append([x_min,y_min,int(box_width),int(box_height)])
+                confidences.append(float(confidence_current))
+                class_numbers.append(class_current)
+
+    result = cv2.dnn.NMSBoxes(bounding_boxes,confidences,
+                              minimum_probability,threshold)
+
+    if len(result) > 0:
+        for i in result.flatten():
+            x_min,y_min = bounding_boxes[i][0], bounding_boxes[i][1]
+            box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
+
+            colour_box_current = colours[class_numbers[i]].tolist()
+            cv2.rectangle(frame,(x_min,y_min), (x_min+box_width,y_min+box_height),
+                          colour_box_current,2)
+            text_box_current = '{}: {:.4f}'.format(labels[int(class_numbers[i])],
+                                                   confidences[i])
+            cv2.putText(frame, text_box_current, (x_min, y_min - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour_box_current, 2)
+    if writer is None:
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        writer = cv2.VideoWriter('videos/result-traffic-cars.mp4',fourcc,30,
+                                 (frame.shape[1],frame.shape[0]),True)
+
+    writer.write(frame)
